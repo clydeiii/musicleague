238acfabb8def07da5e03cbe1ef0547d4593f3d9
Clyde Wright cwwrigh <cwwrigh@ip-10-114-151-6.evoforge.org>
2022-06-07 19:41:39 +0000

    added picky and influential scores

--- mlanalysis.py
+++ mlanalysis.py
@@ -10,7 +10,7 @@ submissions_by_round = {}
 round_winners = {}
 round_losers = {}
 
-league_name = 'mazala-q2'
+league_name = 'mazala-q3'
 if len(sys.argv) > 1:
     league_name = sys.argv[1]
 
@@ -25,7 +25,7 @@ with open(f"data/{league_name}/rounds.csv", newline='') as csvfile:
 with open(f"data/{league_name}/competitors.csv", newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        competitors[row['ID']] = {'name': row['Name'], 'submissions': {}, 'votes' : {}, 'chatty_score': 0, 'sheep_score': 0}
+        competitors[row['ID']] = {'name': row['Name'], 'submissions': {}, 'votes' : {}, 'chatty_score': 0, 'sheep_score': 0, 'picky_score': 0, 'taste_maker': 0, 'taste_faker': 0}
         for round_id in rounds:
             competitors[row['ID']]['votes'][round_id] = {}
 
@@ -39,46 +39,56 @@ with open(f"data/{league_name}/votes.csv", newline='') as csvfile:
     for row in reader:
         if int(row['Points Assigned']) > 0: 
             competitors[row['Voter ID']]['votes'][row['Round ID']][row['Spotify URI']] = int(row['Points Assigned'])
-        else: # if they awarded 0 points it means they left a comment but no points, give them chatty points
-            competitors[row['Voter ID']]['chatty_score'] += 1
+            competitors[row['Voter ID']]['picky_score'] += 1 # award them a picky point for voting for a song
+        else: 
+            competitors[row['Voter ID']]['chatty_score'] += 1 # if they awarded 0 points it means they left a comment but no points, give them chatty points
         submissions_by_round[row['Round ID']][row['Spotify URI']] += int(row['Points Assigned'])
 
 for round in rounds:
     round_winners[round] = max(submissions_by_round[round], key=submissions_by_round[round].get)
     round_losers[round] = min(submissions_by_round[round], key=submissions_by_round[round].get)
 
-
+print("\nchatty score is the number of times you left a comment on a song you awarded zero points to")
 for competitor in competitors.values():
     print(f"{competitor['name']}'s chatty score: {competitor['chatty_score']}")
-#print(json.dumps(competitors['43c8053c706643709c37d432b5118001'], indent = 4)) 
-#print(json.dumps(round_winners, indent = 4))
-#quit()
+
+print("\npicky score is the number of times you awarded a song any number of points (the lower the score, the pickier)")
+for competitor in competitors.values():
+    print(f"{competitor['name']}'s picky score: {competitor['picky_score']}")
+
 
 # get taste maker and taste faker (who most often voted for the "worst" song)
-taste_makers = DefaultDict(int)
-taste_fakers = DefaultDict(int)
 for competitor in competitors.values():
     for round in rounds:
         # did they vote for the first place song?
         if any(round_winners[round] in songs for songs in competitor['votes'][round]):
-            taste_makers[competitor['name']] += competitor['votes'][round][round_winners[round]] #give them taste maker points equal to the amount of votes they gave the winning song
+            competitor['taste_maker'] += competitor['votes'][round][round_winners[round]] #give them taste maker points equal to the amount of votes they gave the winning song
         # did they vote for the last place song?
         if any(round_losers[round] in songs for songs in competitor['votes'][round]):
-            taste_fakers[competitor['name']] += competitor['votes'][round][round_losers[round]]
+            competitor['taste_faker'] += competitor['votes'][round][round_losers[round]]
         # loop through all of the song submisisons this round and award this competitor sheep points for voting with others
         for submission in submissions_by_round[round]:
             # did this user vote for this submission?
             if submission in competitor['votes'][round]:
                 competitor['sheep_score'] += submissions_by_round[round][submission] * competitor['votes'][round][submission] # all points assigned this round to this submisison multiplied by how many points this person gave it
 
-
+print("\nsheep score is how often you voted with the herd")
 for competitor in competitors.values():
     print(f"{competitor['name']}'s sheep score: {competitor['sheep_score']}")
 
-print("Taste Maker Scores:")
-print(json.dumps(taste_makers, indent = 4))
-print("Taste Faker Scores:")
-print(json.dumps(taste_fakers, indent = 4))
+print('\nmost influential = (1/picky) x sheep x tastemaker')
+for competitor in competitors.values():
+    print(f"{competitor['name']}'s influence score score: {1/competitor['picky_score'] * competitor['sheep_score'] * competitor['taste_maker']}")
+
+
+print("\nTaste Maker Scores (who most often voted for the best song):")
+for competitor in competitors.values():
+    print(f"{competitor['name']}'s TasteMaker score: {competitor['taste_maker']}")
+#print(json.dumps(taste_makers, indent = 4))
+print("\nTaste Faker Scores (who most often voted for the worst song):")
+for competitor in competitors.values():
+    print(f"{competitor['name']}'s taste faker: {competitor['taste_faker']}")
+#print(json.dumps(taste_fakers, indent = 4))
 #quit()
 
 # find 'how many points did i assign to each other player and in how many rounds'
@@ -101,11 +111,12 @@ for competitor in competitors.values():
                             if(votes_for_song > 0):
                                 who_they_voted_for[other_player['name']][1] += 1
     sorted_voting_history = sorted(who_they_voted_for.items(), key=lambda item: item[1])
-    print(f"{competitor['name']}'s voting history ({total_points_assigned}): {sorted_voting_history}")
+    #print(f"{competitor['name']}'s voting history ({total_points_assigned}): {sorted_voting_history}")
 
 #quit()
 
 # find each competitor's "biggest fan" and "nemesis"
+print("\nbiggest fan and nemesis")
 for competitor in competitors.values():
     biggest_fans = {}
     score = 0
@@ -126,7 +137,7 @@ for competitor in competitors.values():
                             if(points_received > 0):
                                 biggest_fans[possible_fan['name']][1] += 1 # tally how rounds they voted for this person (regardless of points assigned)
     sorted_voting_history = sorted(biggest_fans.items(), key=lambda item: item[1])
-    print(f"{competitor['name']}'s voted for history ({score}): {sorted_voting_history}")
+    #print(f"{competitor['name']}'s voted for history ({score}): {sorted_voting_history}")
     print(f"{competitor['name']}'s biggest fan is {sorted_voting_history[len(sorted_voting_history)-1]} and their nemesis is {sorted_voting_history[0]}")
 
 
